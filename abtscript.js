4384ba93a75e03aca604be7c9d74f8d3fd9de945
cctansh <s10260078@connect.np.edu.sg>
2023-11-23 00:12:18 +0800

    added abt js for image swapping

---
+++ abtscript.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var mainImg = document.getElementById('main');
+
+    document.getElementById('prev1').onclick = function() {
+        mainImg.src = 'imgs/preview1.png';
+    };
+    document.getElementById('prev2').onclick = function() {
+        mainImg.src = 'imgs/preview2.png';
+    };
+    document.getElementById('prev3').onclick = function() {
+        mainImg.src = 'imgs/preview3.png';
+    };
+    document.getElementById('prev4').onclick = function() {
+        mainImg.src = 'imgs/preview4.png';
+    };
+};
